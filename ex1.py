5a950f68e732749f764284bfce20bae98d1971a9
ChenJingjie <424158770@qq.com>
2017-09-09 16:37:15 +0800

    第一个程序

---
+++ ex1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+##################
+# exercise1: 第一个程序
+##################
+print "Hello World!"
+print '你好'
+print u'你好，python'#win下需要添加u
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! printing.'
+print "I`d much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# 输出中文的时候要在字符串前面加u。
+# 因为python中字符串默认采用ASCII，要声明为unicode就要加u(linux环境下不需要加u)
