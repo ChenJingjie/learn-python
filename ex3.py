a77ac7f9dc94e709fbc9ba3ffa5395335a31808a
ChenJingjie <424158770@qq.com>
2017-09-10 23:56:25 +0800

    数字和数学计算

---
+++ ex3.py
@@ -0,0 +1,37 @@
+#coding: utf-8
+#################
+# 习题3:数字和数学计算
+#################
+print "I will now count my chickens:" # 我要数数我的鸡
+
+print u"Hens，母鸡", 25 + 30 / 6
+print u"Rootsters，公鸡", 100 - 25 * 3 % 4
+
+print u"Now I will count eggs:，(现在我要数鸡蛋了:)"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5-7
+
+print "what is 3 + 2?", 3 + 2
+print "what  is 5 - 7", 5 - 7
+
+print "Oh,what`s why it`s False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is ite greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+#笔记
+# 1.浮点数概念，应该写成：
+print u"浮点数应该这样写"
+print "20.0 / 3 = ", 20.0 / 3
+# 2. 除法结果
+print u"对比下."
+print "7 / 6 < 7.0 / 6.0", 7 / 6 < 7.0 / 6.0
+print "7 / 6 = ", 7 / 6
+print "7.0 / 6.0 = ", 7.0 / 6.0
