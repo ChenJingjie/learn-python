22ef83b679e28fe63acc3b6d469535cdc467334b
ChenJingjie <424158770@qq.com>
2017-10-04 16:27:34 +0800

    更多的变量和打印

---
+++ ex5.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+################
+# 习题5：更多变量和打印
+################
+my_name = 'Chen Jingjie'
+my_age = '24'
+my_height = 172
+my_weight = 52
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'black'
+
+print "Let`s talk about %s." % my_name
+print "He`s %d inches tall." % my_height
+print "He`s %d pounds heavy." % my_weight
+print "Actually that`s not too heavy."
+print "He`s got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
