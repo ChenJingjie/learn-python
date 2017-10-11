af9ea6190d7d61c87d09690a96539875fefcd700
ChenJingjie <424158770@qq.com>
2017-10-11 23:27:19 +0800

    更多的打印

---
+++ ex7.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+###################
+# 习题7: 更多的打印
+# 巩固输出 练习
+################### 
+print "Mary had a little lamb." # 羊羔
+print "Its fleece was white as %s." % 'snow'# 羊毛
+print "And everywhere that Mary went."
+print "." * 10 # 输出了十次
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# 加逗号: 俩句在同一行
+#不加逗号: 不同行
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
