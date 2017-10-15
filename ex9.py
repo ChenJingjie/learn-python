f4746d81bada5bca044385dc531c295ec070cf96
ChenJingjie <424158770@qq.com>
2017-10-15 22:13:05 +0800

    打印，打印，打印

---
+++ ex9.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+###################
+# 习题9：打印，打印，打印
+###################
+# Here`s some new strange stuff, remember type it exactly
+# (这里有一些新的奇怪的东西，请记住)
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+ There`s something going on there.
+ With the three double-quotes.
+ We`ll be able to type as much we like.
+ Even 4 lines if we want, or 5, or 6.
+ """
+
+ # 笔记
+ # \n 是换行
+ # 三个双引号包含可以多行存在
